3701a5c69a6e71b9bcc02c4f8fd80663dd746b2f
DareFox <easymalink@gmail.com>
2022-03-30 01:27:02 +0300

    True init commit

---
+++ build.gradle.kts
@@ -0,0 +1,34 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+    kotlin("jvm") version "1.6.10"
+    application
+}
+
+group = "me.darefox"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+val ktor_version = "1.6.8"
+
+dependencies {
+    testImplementation(kotlin("test"))
+    implementation("io.ktor:ktor-client-core:$ktor_version")
+    implementation("io.ktor:ktor-client-cio:$ktor_version")
+    implementation("io.github.resilience4j:resilience4j-ratelimiter:1.7.1")
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
+
+tasks.withType<KotlinCompile> {
+    kotlinOptions.jvmTarget = "1.8"
+}
+
+application {
+    mainClass.set("MainKt")
+}
---
+++ src/main/kotlin/cmtt/base/OsnovaAPI.kt
@@ -0,0 +1,30 @@
+package cmtt.base
+
+import io.github.resilience4j.ratelimiter.RateLimiterConfig
+import io.github.resilience4j.ratelimiter.RateLimiterRegistry
+import io.ktor.client.*
+import java.io.Closeable
+import java.time.Duration
+import java.util.*
+
+abstract class OsnovaAPI(token: String): Closeable {
+    abstract val serverURL: String
+
+    private val rateLimitConfig = RateLimiterConfig
+        .custom()
+        .limitRefreshPeriod(Duration.ofSeconds(1))
+        .limitForPeriod(3)
+        .timeoutDuration(Duration.ofHours(100))
+        .build()
+
+    private val rateLimitID = UUID.randomUUID().toString()
+    private val rateLimiterRegistry = RateLimiterRegistry.of(rateLimitConfig)
+    private val rateLimiter = rateLimiterRegistry.rateLimiter(rateLimitID)
+
+    private val client = HttpClient()
+
+    override fun close() {
+        client.close()
+        rateLimiterRegistry.remove(rateLimitID)
+    }
+}
