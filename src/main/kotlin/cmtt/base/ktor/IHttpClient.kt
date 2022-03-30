9fdcfdde66219f38f0b647fa9b8938f7b37a2214
DareFox <easymalink@gmail.com>
2022-03-30 16:59:24 +0300

    Create adapter for Ktor
    To control rate limit and create proxy classes (e.g. cached client and etc)

---
+++ src/main/kotlin/cmtt/base/ktor/IHttpClient.kt
@@ -0,0 +1,34 @@
+package cmtt.base.ktor
+
+import io.github.resilience4j.kotlin.ratelimiter.executeSuspendFunction
+import io.github.resilience4j.ratelimiter.RateLimiter
+import io.ktor.client.*
+import io.ktor.client.engine.*
+import io.ktor.client.request.*
+import io.ktor.client.statement.*
+import io.ktor.util.*
+import kotlinx.coroutines.CoroutineScope
+import java.io.Closeable
+
+interface IHttpClient : CoroutineScope, Closeable {
+    val client: HttpClient
+    val rateLimiter: RateLimiter
+
+    val requestPipeline: HttpRequestPipeline
+    val responsePipeline: HttpResponsePipeline
+    val sendPipeline: HttpSendPipeline
+    val receivePipeline: HttpReceivePipeline
+    val attributes: Attributes
+    val engineConfig: HttpClientEngineConfig
+
+    fun isSupported(capability: HttpClientEngineCapability<*>): Boolean
+    fun config(block: HttpClientConfig<*>.() -> Unit): io.ktor.client.HttpClient
+    override fun toString(): String
+}
+
+suspend inline fun <reified T> IHttpClient.request(crossinline block: HttpRequestBuilder.() -> Unit): T {
+    return rateLimiter.executeSuspendFunction {
+        client.request(block)
+
+    }
+}
