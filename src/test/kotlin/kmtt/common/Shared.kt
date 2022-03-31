961ff5d63f7a46acfef966166f3418b0bd439f94
DareFox <easymalink@gmail.com>
2022-03-31 22:27:46 +0300

    Add tests

---
+++ src/test/kotlin/kmtt/common/Shared.kt
@@ -0,0 +1,32 @@
+package kmtt.common
+
+import io.github.resilience4j.ratelimiter.RateLimiterConfig
+import io.ktor.client.*
+import io.ktor.client.features.json.*
+import io.ktor.client.features.json.serializer.*
+import kmtt.base.ktor.HttpClientAdapter
+import kmtt.base.ktor.IHttpClient
+import java.time.Duration
+
+object Shared {
+    val httpClient: IHttpClient
+    val baseUrl: String = "https://api.dtf.ru/v1.9"
+    val token: String = ""
+
+    init {
+        val rateLimitConfig = RateLimiterConfig
+            .custom()
+            .limitRefreshPeriod(Duration.ofSeconds(1))
+            .limitForPeriod(3)
+            .timeoutDuration(Duration.ofHours(100))
+            .build()
+
+        httpClient = HttpClientAdapter(HttpClient() {
+            install(JsonFeature) {
+                serializer = KotlinxSerializer(kotlinx.serialization.json.Json {
+                    ignoreUnknownKeys = true
+                })
+            }
+        }, rateLimitConfig)
+    }
+}
