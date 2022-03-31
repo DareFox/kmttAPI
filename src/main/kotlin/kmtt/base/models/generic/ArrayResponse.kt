5c186d5a1651d16e1bb92a7e050cd684b0d7cb2f
DareFox <easymalink@gmail.com>
2022-03-31 22:29:26 +0300

    add ArrayResponse Class
    Some requests returns not "result: [array]", but "result: { items: [array] }"

---
+++ src/main/kotlin/kmtt/base/models/generic/ArrayResponse.kt
@@ -0,0 +1,7 @@
+package kmtt.base.models.generic
+
+@kotlinx.serialization.Serializable
+data class ArrayResponse <T>(val items: T)
+
+@kotlinx.serialization.Serializable
+data class SuccessArrayResponse<T>(val result: ArrayResponse<T>)
