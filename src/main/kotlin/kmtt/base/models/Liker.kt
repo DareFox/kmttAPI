d9f50038f98fe2c33fe5a3106a4fc72b6ece7a2c
DareFox <easymalink@gmail.com>
2022-03-31 22:31:42 +0300

    add SerialName to avatarURL in Liker Class

--- src/main/kotlin/kmtt/base/models/Liker.kt
+++ src/main/kotlin/kmtt/base/models/Liker.kt
@@ -1,10 +1,18 @@
 package kmtt.base.models
 
+import kotlinx.serialization.KSerializer
 import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializer
+import kotlinx.serialization.descriptors.PrimitiveKind
+import kotlinx.serialization.descriptors.PrimitiveSerialDescriptor
+import kotlinx.serialization.descriptors.SerialDescriptor
+import kotlinx.serialization.encoding.Decoder
+import kotlinx.serialization.encoding.Encoder
 
 @kotlinx.serialization.Serializable
 data class Liker(
     @SerialName("sign") val vote: Int,
     val name: String,
-    val avatarURL: String
+    @SerialName("avatar_url") val avatarURL: String
 )
+
