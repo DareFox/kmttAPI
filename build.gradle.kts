b0c1aa459748b4380bebb81a5e7abdf056e3f117
DareFox <easymalink@gmail.com>
2022-04-03 15:00:25 +0300

    Change build version and prepare gradle for lib build

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,12 +1,14 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
+    id("java-library")
+    id("maven-publish")
     kotlin("jvm") version "1.6.10"
     kotlin("plugin.serialization") version "1.6.10"
 }
 
 group = "me.darefox"
-version = "1.0-SNAPSHOT"
+version = "0.1.0"
 
 repositories {
     mavenCentral()
@@ -16,9 +18,11 @@ val ktor_version = "1.6.8"
 
 dependencies {
     testImplementation(kotlin("test"))
+
     implementation("io.ktor:ktor-client-core:$ktor_version")
     implementation("io.ktor:ktor-client-cio:$ktor_version")
     implementation("io.ktor:ktor-client-serialization:$ktor_version")
+
     implementation("io.github.resilience4j:resilience4j-ratelimiter:1.7.1")
     implementation("io.github.resilience4j:resilience4j-kotlin:1.7.1")
     implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.3.2")
@@ -32,7 +36,3 @@ tasks.test {
 tasks.withType<KotlinCompile> {
     kotlinOptions.jvmTarget = "1.8"
 }
-
-//application {
-//    mainClass.set("MainKt")
-//}
