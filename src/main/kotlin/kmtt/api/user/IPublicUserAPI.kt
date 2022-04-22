5f7c2f4cbfc281a001c7be07272ac6a91474ef77
DareFox <easymalink@gmail.com>
2022-04-22 05:45:16 +0300

    UserAPI: add docs

--- src/main/kotlin/kmtt/api/user/IPublicUserAPI.kt
+++ src/main/kotlin/kmtt/api/user/IPublicUserAPI.kt
@@ -7,29 +7,66 @@ import kmtt.models.entry.Entry
 import kmtt.models.subsite.Subsite
 
 interface IPublicUserAPI {
+    /**
+     * Получить информацию о пользователе
+     */
     suspend fun getUserByID(userID: Long): Subsite
 
+    /**
+     * Получить комментарии пользователя
+     */
     suspend fun getUserComments(userID: Long, count: Int, offset: Int): List<Comment>
 
+    /**
+     * Получить записи пользователя
+     */
     suspend fun getUserEntries(userID: Long, count: Int, offset: Int): List<Entry>
 }
 
 interface IAuthUserAPI : IPublicUserAPI, Authable {
+
+    /**
+     * Получить информацию о текущем пользователе
+     */
     suspend fun getMe(): Subsite
 
+    /**
+     * Получить список уведомлений
+     */
     suspend fun getMyNotifications(): List<Notification>
 
+    /**
+     * Получить количество непрочитанных уведомлений у текущего пользователя
+     */
     suspend fun getMyNotificationsCount(): Int
 
+    /**
+     * Сделать уведомление прочитанным
+     */
     suspend fun readNotificationsById(id: Long): Boolean
 
+    /**
+     * Сделать уведомления прочитанными
+     */
     suspend fun readAllNotifications(): List<Notification>
 
+    /**
+     * Получить комментарии текущего пользователя
+     */
     suspend fun getMyComments(count: Int, offset: Int): List<Comment>
 
+    /**
+     * Получить записи текущего пользователя
+     */
     suspend fun getMyEntries(count: Int, offset: Int): List<Entry>
 
+    /**
+     * Получить избранные записи текущего пользователя
+     */
     suspend fun getMyFavoriteEntries(count: Int, offset: Int): List<Entry>
 
+    /**
+     * Получить избранные комментарии текущего пользователя
+     */
     suspend fun getMyFavoriteComments(count: Int, offset: Int): List<Comment>
 }
