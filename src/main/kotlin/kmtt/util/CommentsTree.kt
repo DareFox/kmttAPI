5ad56a2e805bde4d06ad74bdef0186b41030ddb4
DareFox <easymalink@gmail.com>
2022-06-13 11:59:48 +0300

    Comments: Add utility to convert them to a node tree

---
+++ src/main/kotlin/kmtt/util/CommentsTree.kt
@@ -0,0 +1,41 @@
+package kmtt.util
+
+import kmtt.models.comment.Comment
+
+fun Collection<Comment>.toTree(): List<CommentNode> {
+    val mutableList = this.toMutableList()
+    val topLevelComments = mutableList.filter { it.replyTo == 0L || it.replyTo == null }
+    val replyMap = mutableList.groupBy { it.replyTo }
+
+    return topLevelComments.map {
+        createRecursiveNode(it, replyMap)
+    }
+}
+
+private fun createRecursiveNode(value: Comment, replyMap: Map<Long?, List<Comment>>, parent: CommentNode? = null): CommentNode {
+    val childrenNodes = mutableListOf<CommentNode>()
+
+    val node = CommentNode(
+        parent = parent,
+        nodeValue = value
+    )
+
+    replyMap[value.id]?.forEach { comment ->
+        childrenNodes += createRecursiveNode(comment, replyMap, node)
+    }
+
+    // Add children nodes before return it
+    return node.apply {
+        _children = childrenNodes
+    }
+}
+
+
+data class CommentNode(
+    val parent: CommentNode?,
+    val nodeValue: Comment,
+) {
+    internal var _children: List<CommentNode> = listOf()
+    val children: List<CommentNode>
+        get() = _children
+}
