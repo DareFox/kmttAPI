d3dfbb1b7823c0712dd5aa25b1009d57e81e5afc
darefox <easymalink@gmail.com>
2022-08-24 02:50:27 +0300

    CommentsTree: Remove parent and make function use iterations
    When you try to serialize object, it will throw  StackOverflow error. That's because parent value will not be reference, but actual value and serializer will get into infinite loop of recursion
    Also to prevent StackOverflow exception from cause of deep tree, I changed  recursive calls to iterations

--- src/main/kotlin/kmtt/util/CommentsTree.kt
+++ src/main/kotlin/kmtt/util/CommentsTree.kt
@@ -1,40 +1,61 @@
 package kmtt.util
 
 import kmtt.models.comment.Comment
+import kotlinx.coroutines.*
+import kotlinx.coroutines.sync.Mutex
+import kotlinx.coroutines.sync.withLock
+import kotlinx.serialization.Serializable
+import kotlinx.serialization.json.JsonNames
 
 fun Collection<Comment>.toTree(): List<CommentNode> {
-    val topLevelComments = this.filter { it.replyTo == 0L || it.replyTo == null }
     val replyMap = this.groupBy { it.replyTo }
+    val topZeroComments = replyMap[0] ?: listOf()
+    val topNullComments = replyMap[null] ?: listOf()
+    val topLevelComments = topZeroComments + topNullComments
 
     return topLevelComments.map {
         createRecursiveNode(it, replyMap)
     }
 }
 
-private fun createRecursiveNode(value: Comment, replyMap: Map<Long?, List<Comment>>, parent: CommentNode? = null): CommentNode {
-    val childrenNodes = mutableListOf<CommentNode>()
-
+private fun createRecursiveNode(value: Comment, replyMap: Map<Long?, List<Comment>>): CommentNode {
     val node = CommentNode(
-        parent = parent,
-        nodeValue = value
+        value = value
     )
 
-    replyMap[value.id]?.forEach { comment ->
-        childrenNodes += createRecursiveNode(comment, replyMap, node)
+    var currentLayer = replyMap[value.id]?.map {
+        CommentNode(it)
+    } ?: listOf()
+
+    node.children = currentLayer
+
+    while (currentLayer.isNotEmpty()) {
+        val nextLayer = mutableListOf<CommentNode>()
+
+        for (nodeParent in currentLayer) {
+            val id = nodeParent.value.id
+            val nodeChildren = if (id == null) listOf() else {
+                replyMap[id]?.map {
+                    CommentNode(it)
+                } ?: listOf()
+            }
+            
+            nodeParent.children = nodeChildren
+            nextLayer += nodeChildren
+        }
+
+        currentLayer = nextLayer
     }
 
-    // Add children nodes before return it
-    return node.apply {
-        _children = childrenNodes
-    }
+    return node
 }
 
-
+@Serializable
 data class CommentNode(
-    val parent: CommentNode?,
-    val nodeValue: Comment,
+    val value: Comment,
+    @JsonNames("children")
+    var children: List<CommentNode> = listOf<CommentNode>()
 ) {
-    internal var _children: List<CommentNode> = listOf()
-    val children: List<CommentNode>
-        get() = _children
+    val nodeChildren: List<CommentNode>
+        get() { return children }
 }
